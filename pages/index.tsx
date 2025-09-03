beae9eea36bd715a6e8b22231bb9dc0d32a1d2b0
akshai-m <mylapuramakshaikumar@gmail.com>
2025-09-03 19:25:24 +0530

    feat:: index page

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import Header from "@/app/components/Header";
+import "@/app/globals.css"
+const Index = () => {
+  return (
+    <div className="min-h-screen">
+      <Header />
+    </div>
+  );
+};
+
+export default Index;
