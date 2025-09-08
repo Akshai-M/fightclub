332b64b82dfdcbfbab1fa553c59daa3cb62365c3
akshai-m <mylapuramakshaikumar@gmail.com>
2025-09-08 20:14:32 +0530

    refactor:: Changes in the Header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/app/components/ui/button";
 
 const Header = () => {
   return (
-    <header className="fixed top-0 w-full z-50 bg-background/80 backdrop-blur-md border-b border-border/20 px-14">
+    <header className="fixed top-0 w-full z-50 bg-white border-b px-14">
       <div className="container mx-auto px-4 py-4 flex items-center justify-between">
         <div className="flex items-center space-x-3">
           <div className="w-12 h-12 rounded-xl flex items-center justify-center font-bold text-primary-foreground text-xl text-white bg-[#f82a3b]">
@@ -10,46 +10,46 @@ const Header = () => {
           </div>
 
           <div>
-            <h1 className="text-xl font-bold text-white">Fightclub India</h1>
-            <p className="text-sm text-zinc-200/70">
+            <h1 className="text-xl font-bold">Fightclub India</h1>
+            <p className="text-sm  text-neutral-900">
               An Extension of Chennai Martial Arts
             </p>
           </div>
         </div>
-        <nav className="hidden md:flex items-center space-x-6">
+        <nav className="hidden md:flex font-medium items-center space-x-6">
           <a
-            href="#products"
-            className="text-white hover:text-primary martial-transition"
+            href="#services"
+            className=" hover:text-primary transition-colors duration-300 ease-in-out"
           >
             Products
           </a>
           <a
-            href="#training"
-            className="text-white hover:text-primary martial-transition"
+            href="#services"
+            className=" hover:text-primary transition-colors duration-300 ease-in-out"
           >
             Training
           </a>
           <a
-            href="#workshops"
-            className="text-white hover:text-primary martial-transition"
+            href="#services"
+            className=" hover:text-primary transition-colors duration-300 ease-in-out"
           >
             Workshops
           </a>
           <a
             href="#about"
-            className="text-white hover:text-primary martial-transition"
+            className=" hover:text-primary transition-colors duration-300 ease-in-out"
           >
             About
           </a>
           <a
             href="#contact"
-            className="text-white hover:text-primary martial-transition"
+            className=" hover:text-primary transition-colors duration-300 ease-in-out"
           >
             Contact
           </a>
-          <Button variant="martial" size="sm" className="bg-[#f82a3b] text-white rounded-[10px]">
+          {/* <Button variant="martial" size="sm" className="bg-[#f82a3b] text-white rounded-[10px]">
             Your Orders
-          </Button>
+          </Button> */}
         </nav>
       </div>
     </header>
